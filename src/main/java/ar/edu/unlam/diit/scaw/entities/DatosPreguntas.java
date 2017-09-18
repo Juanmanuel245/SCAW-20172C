49d22f0f5999f6e0f69ff4a79a18a5732877cd58
Brian <briankuz.l@gmail.com>
2017-09-18 00:22:36 -0300

    Se realizo logica de Preguntas

---
+++ src/main/java/ar/edu/unlam/diit/scaw/entities/DatosPreguntas.java
@@ -0,0 +1,33 @@
+package ar.edu.unlam.diit.scaw.entities;
+
+public class DatosPreguntas {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private Integer idExamen;
+	private String pregunta;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getIdExamen() {
+		return idExamen;
+	}
+	public void setIdExamen(Integer idExamen) {
+		this.idExamen = idExamen;
+	}
+	public String getPregunta() {
+		return pregunta;
+	}
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
